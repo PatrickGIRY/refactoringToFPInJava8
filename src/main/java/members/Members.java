a274ac7bc4af31fa73dc7a4407e6d05eef0e4058
Patrick GIRY <patrickgiry@gmail.com>
2018-10-15 12:06:33 +0200

    Transform to filter / map / reduce algo.

--- src/main/java/members/Members.java
+++ src/main/java/members/Members.java
@@ -2,7 +2,6 @@ package members;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
 class Members {
@@ -14,25 +13,15 @@ class Members {
 
     Participants findParticipantsByFirstName(String query) {
         final Predicate<Member> predicate = matches(query);
-        final BiFunction<List<Participant>, Participant, List<Participant>> add = (participants, participant) -> {
-            participants.add(participant);
-            return participants;
-        };
 
         final List<Participant> participants = new ArrayList<>();
         for (Member member : members) {
-            addIf(predicate, member, add).apply(participants, member.toParticipant());
+            if (predicate.test(member)) {
+                participants.add(member.toParticipant());
+            }
         }
 
         return Participants.of(participants);
-    }
-
-    private BiFunction<List<Participant>, Participant, List<Participant>> addIf(Predicate<Member> predicate, Member member, BiFunction<List<Participant>, Participant, List<Participant>> add) {
-        if (predicate.test(member)) {
-            return add;
-        } else {
-            return (ps, p) -> ps;
-        }
     }
 
     private Predicate<Member> matches(String query) {
