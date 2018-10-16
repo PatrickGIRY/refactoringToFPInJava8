710aeef020370bfbf4ed64ef34a38806dea0f883
Patrick GIRY <patrickgiry@gmail.com>
2018-10-16 17:15:04 +0200

    Transform to stream.

--- src/main/java/members/Members.java
+++ src/main/java/members/Members.java
@@ -1,7 +1,9 @@
 package members;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 class Members {
     private final Member[] members;
@@ -11,12 +13,16 @@ class Members {
     }
 
     Participants findParticipantsByFirstName(String query) {
-        final List<Participant> participants = new ArrayList<>();
-        for (Member member : members) {
-            if (member.startWith(query)) {
-                participants.add(member.toParticipant());
-            }
-        }
+
+        List<Participant> participants = Arrays.stream(members)
+                .filter(matches(query))
+                .map(Member::toParticipant)
+                .collect(Collectors.toList());
+
         return Participants.of(participants);
+    }
+
+    private Predicate<Member> matches(String query) {
+        return member -> member.startWith(query);
     }
 }
