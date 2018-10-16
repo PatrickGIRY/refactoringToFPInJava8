788123c01570fe04fa799654f73bdda44fb01683
Patrick GIRY <patrickgiry@gmail.com>
2018-10-16 21:44:36 +0200

    * Currying `append`. * Change `addIf` to call first participant function parameter only and return consumer of list of participants.

--- src/main/java/members/Members.java
+++ src/main/java/members/Members.java
@@ -2,8 +2,9 @@ package members;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 class Members {
     private final Member[] members;
@@ -15,22 +16,23 @@ class Members {
     Participants findParticipantsByFirstName(String query) {
         final BiPredicate<String, Member> predicate = this::matches;
 
-        final BiConsumer<Participant, List<Participant>> append = (participant, participants) -> participants.add(participant);
+        final Function<Participant, Consumer<List<Participant>>> append =
+                participant -> participants -> participants.add(participant);
 
         final List<Participant> participants = new ArrayList<>();
         for (Member member : members) {
-            BiConsumer<Participant, List<Participant>> consumer = addIf(predicate, query, member, append);
-            consumer.accept(member.toParticipant(), participants);
+            Consumer<List<Participant>> consumer = addIf(predicate, query, member, append);
+            consumer.accept(participants);
         }
 
         return Participants.of(participants);
     }
 
-    private BiConsumer<Participant, List<Participant>> addIf(BiPredicate<String, Member> predicate, String query, Member member, BiConsumer<Participant, List<Participant>> append) {
+    private Consumer<List<Participant>> addIf(BiPredicate<String, Member> predicate, String query, Member member, Function<Participant, Consumer<List<Participant>>> append) {
         if (predicate.test(query, member)) {
-            return append;
+            return append.apply(member.toParticipant());
         } else {
-            return (participant, participants) -> {
+            return (participants) -> {
             };
         }
     }
