8bc216fd6c469acbdf09634dd53f1b04c477b664
Patrick GIRY <patrickgiry@gmail.com>
2018-10-16 20:52:21 +0200

    * Create a predicate with `matches`. * Add `predicate` as first parameter of `addIfMatches`. * Use `predicate` instead of `matches` in `addIfMatches`.

--- src/main/java/members/Members.java
+++ src/main/java/members/Members.java
@@ -2,6 +2,7 @@ package members;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiPredicate;
 
 class Members {
     private final Member[] members;
@@ -11,15 +12,17 @@ class Members {
     }
 
     Participants findParticipantsByFirstName(String query) {
+        final BiPredicate<String, Member> predicate = this::matches;
+
         final List<Participant> participants = new ArrayList<>();
         for (Member member : members) {
-            addIfMatches(query, member, participants);
+            addIfMatches(predicate, query, member, participants);
         }
         return Participants.of(participants);
     }
 
-    private void addIfMatches(String query, Member member, List<Participant> participants) {
-        if (matches(query, member)) {
+    private void addIfMatches(BiPredicate<String, Member> predicate, String query, Member member, List<Participant> participants) {
+        if (predicate.test(query, member)) {
             participants.add(member.toParticipant());
         }
     }
